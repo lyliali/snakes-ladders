ab900937abcb13a3bac53921dadd37e038161b3e
Lylia Li <lyliax@gmail.com>
2023-10-02 13:48:07 -0700

    match behance design

--- app/js/board.js
+++ app/js/board.js
@@ -1,8 +1,10 @@
 const board = [];
-const size = 10;
+// Let board[0] be the player's location
+const size = 101;
 
 function populate(arr, size) {
-    for (let i = 0; i < size; i++) {
+    arr.push(1);
+    for (let i = 1; i < size; i++) {
         // Option 1: Regular tile, return i + 1
         arr.push(i + 1);
         // Option 2: Snake, return number between 1 and i
@@ -12,7 +14,23 @@ function populate(arr, size) {
 
 populate(board, size);
 
-let player = 1;
+// Match the Behance design; tile indices are off by 1
+function customize(arr) {
+    arr[8] = 31;
+    arr[17] = 39;
+    arr[28] = 75;
+    arr[36] = 15;
+    arr[37] = 57;
+    arr[58] = 85;
+    arr[62] = 42;
+    arr[68] = 87;
+    arr[70] = 71;
+    arr[84] = 48;
+    arr[92] = 72;
+    arr[99] = 81;
+}
+
+customize(board);
 
 function roll() {
     return 1 + Math.floor(Math.random() * 6);
@@ -21,12 +39,12 @@ function roll() {
 function takeTurn() {
     let r = roll();
     console.log("I rolled " + r);
-    let next = player + r;
+    let next = board[0] + r;
     if (next <= size) {
-        player = next;
+        board[0] = next;
     }
     if (next === size) {
         console.log("I win!");
     }
-    return player;
+    return board[0];
 }
